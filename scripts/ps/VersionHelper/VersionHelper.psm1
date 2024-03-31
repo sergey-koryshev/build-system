1b0c62970c639fb7e5ded266cfb7fd9cd65f7bae
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-03-31 11:46:46 +0200

    Revert "DUMMY CHANGE (NEED TO REVERT)"
    This reverts commit 622086f88ffc7331e2afb29d49bed32a4cfddf02.

--- scripts/ps/VersionHelper/VersionHelper.psm1
+++ scripts/ps/VersionHelper/VersionHelper.psm1
@@ -484,7 +484,6 @@ function Set-IncrementedVersion {
   Submit-NewVersionLabel -ProjectType Posh -PowerShellModuleName C:\Modules\MyModule.psd1 -SHA "abcdef..." -Owner "Alex" -Repository "WarfaceAim" -DefaultIncrementingPart "Revision" -VersionConfigurationPath "C:\version-configuration.json" -AuthToken "abcdef..."
 .OUTPUTS
   Returns incremented string version.
-  Dummy change
 #>
 function Submit-NewVersionLabel {
   [CmdletBinding()]
